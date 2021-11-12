b4b2695b88117a478dc8fbd1e7cf939d324ae07a
lkb2k <lorin@vinoshipper.com>
2021-11-11 18:05:55 -0800

    Initial Commit

---
+++ src/main/kotlin/com/vinoshipper/exercise/VsExerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.vinoshipper.exercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VsExerciseApplication
+
+fun main(args: Array<String>) {
+	runApplication<VsExerciseApplication>(*args)
+}
---
+++ src/test/kotlin/com/vinoshipper/exercise/VsExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vinoshipper.exercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VsExerciseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
