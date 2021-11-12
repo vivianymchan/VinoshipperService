55dabe0fcfa7c91a51bb8f54d7d2f7ab5b9d071f
lkb2k <lorin@vinoshipper.com>
2021-11-11 18:52:42 -0800

    Fix whitespace.

--- src/main/kotlin/com/vinoshipper/exercise/api/HelloWorldController.kt
+++ src/main/kotlin/com/vinoshipper/exercise/api/HelloWorldController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 data class HelloForm(
-    val name:String
+    val name: String
 )
 
 data class HelloResponse(
